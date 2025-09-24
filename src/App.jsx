798d825047772649f9839f2f86acb657e8c6c474
Mokarama <mokaramaaktershanta@gmail.com>
2025-09-24 14:04:35 +0600

    Add Experience & Projects section with project cards and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import "./App.css";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
@@ -8,9 +9,15 @@ import Navbar from "./components/Navbar/Navbar";
 import Skills from "./components/Skills/Skills";
 import Work from "./components/Work/Work";
 
-  const dataFetch=fetch('skillsData.json')
+
+const dataFetch=fetch('skillsData.json')
 .then(res=>res.json())
 console.log(dataFetch)
+
+const experienceDataFetch =fetch('experience.json')
+
+.then(res=>res.json())
+console.log(experienceDataFetch);
 
 function App() {
 
@@ -19,8 +26,15 @@ function App() {
      <div className="bg-[#05081f] ">
         <Navbar />
         <About />
-        <Skills dataFetch={dataFetch} />
-        <Experience />
+
+       <Suspense fallback={<h3 className="text-3xl text-red-500 text-center italic">Loading......</h3>}>
+           <Skills dataFetch={dataFetch} />
+       </Suspense>
+
+
+        <Suspense fallback={<h3 className="text-3xl text-red-500">Loading...</h3>}>
+          <Experience experienceDataFetch={experienceDataFetch} />
+        </Suspense>
         <Work />
         <Education />
         <Contact />
---
+++ src/components/ExperienceCard/ExperienceCard.jsx
@@ -0,0 +1,38 @@
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
+const ExperienceCard = ({ experience }) => {
+ const {title,role,tech,description,image,live,github}=experience;
+  return (
+    <div>
+      <div className="card border-2 border-gray-400 rounded-2xl shadow-sm text-white py-5">
+        <figure className="px-10">
+          <img 
+            src={image}
+            alt="projectImg"
+            className="rounded-xl h-[200px] w-full object-cover"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-bold text-xl py-2">{title}</h2>
+          <p className="py-2 px-2">{description}</p>
+          <div>
+             <ul className="md:flex flex-wrap justify-around gap-1 sm:w-full">
+                {
+                    tech.map((item, index)=>(<li className="border-2 bg-gray-700  mb-1  rounded-xl px-2" key={index}>{item}</li>))
+                }
+            </ul>
+          </div>
+          <div className="card-actions mt-5 flex justify-around">
+            <a href='' className=" border-2 border-gray-400 rounded-xl py-2 px-5 text-xl bg-gray-500 ">
+                <FaGithub />
+            </a>
+            <a  className="border-2 border-gray-400 rounded-xl pt-2 px-5 bg-gray-500  ">
+                <FaExternalLinkAlt />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceCard;
